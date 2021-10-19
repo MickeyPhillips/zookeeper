6f1fb6450390ac25aeab6a4724cc92a4d3fe907e
Mickey Phillips <phillipsmickey41@gmail.com>
2021-10-19 14:07:11 -0500

    finished

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const animalRoutes = require('../apiRoutes/animalRoutes.js');
+
+router.use(require('./zookeeperRoutes'));
+router.use(animalRoutes);
+
+module.exports = router;
